8509b13fa0f96a015df4e1e62ec52b0b86a5234c
John Strong <yesac63@gmail.com>
2017-05-09 08:30:55 -0500

    Create server.js

---
+++ server.js
@@ -0,0 +1,10 @@
+var app = require('express')();
+var http = require('http').Server(app);
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+http.listen(1337, function(){
+  console.log('Now listening...');
+});

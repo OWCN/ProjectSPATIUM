c2a2ed20edfd0e8a2a6cd5cec2c50c52bcfb2051
domorecalculus <yesac63@gmail.com>
2017-05-11 01:19:01 -0500

    Basic socket.io integration

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 var app = require('express')();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+    console.log("A user connected.");
+
+    socket.on('disconnect', function () {
+        console.log("A user disconnected.");
+    });
 });
 
 http.listen(1337, function(){

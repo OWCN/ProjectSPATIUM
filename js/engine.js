a606d85e6880d0ee6353ab4305bf10dc537e9135
John Strong <yesac63@gmail.com>
2017-05-13 15:19:59 -0500

    Added asteroid class and types, player class, and ship types

--- js/engine.js
+++ js/engine.js
@@ -1,3 +1,15 @@
+//ENUMS
+var shipType = {
+    STARTER: "Starter Ship"
+};
+
+var asteroidType = {
+    SMALL: "Small Asteroid",
+    MEDIUM: "Medium Asteroid",
+    LARGE: "Large Asteroid"
+};
+
+//CLASSES
 function Vector(x, y, z){
     this.x = x;
     this.y = y;
@@ -14,4 +26,26 @@ function Vector(x, y, z){
         this.y = this.y + vector2.y;
         this.z = this.z + vector2.z;
     };
+}
+
+function Player(posX, posY, posZ, rotX, rotY, rotZ, momX, momY, momZ) {
+    this.position = new Vector(posX, posY, posZ);
+    this.rotation = new Vector(rotX, rotY, rotZ);
+    this.momentum = new Vector(momX, momY, momZ);
+
+    this.shipType = shipType.STARTER;
+
+    this.bullets = [];
+
+    this.resPoints = 0;
+}
+
+function Asteroid(posX, posY, posZ, rotX, rotY, rotZ, momX, momY, momZ) {
+    this.position = new Vector(posX, posY, posZ);
+    this.rotation = new Vector(rotX, rotY, rotZ);
+    this.momentum = new Vector(momX, momY, momZ);
+
+    this.asteroidType = asteroidType.SMALL;
+
+    //ore implementation
 }

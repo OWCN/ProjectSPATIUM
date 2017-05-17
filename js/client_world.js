5dff586ec791bb5b798790f843f81636204b7639
domorecalculus <yesac63@gmail.com>
2017-05-17 18:00:34 -0500

    Added starter ship file and begun client_world

---
+++ js/client_world.js
@@ -0,0 +1,16 @@
+var container, scene, camera, renderer, raycaster, objects = [];
+var keyState = {};
+var playerShip;
+
+var player;
+
+var otherPlayers = [], asteroids = [], bullets = [], ores = [];
+
+var loadWorld = function(){
+    init();
+    animate();
+
+    function init(){
+
+    };
+}
